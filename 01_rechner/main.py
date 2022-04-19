4f1c6a9fae9a46134aa16eaca1ef30b5a5fdf24c
itkurse <89607257+itkurse@users.noreply.github.com>
2022-04-19 14:37:38 +0200

    erstes Beispiel

---
+++ 01_rechner/main.py
@@ -0,0 +1,40 @@
+# # Kommentar
+# '''
+# Ich bin ein mehrzeiliger Kommentar
+# '''
+
+# Erstelle eine Int-Variable für x mit dem Startwert 0
+x = 0
+# Erstelle eine Int-Variable für y mit dem Startwert 0
+y = 0
+# Erstelle eine Int-Variable für summe mit dem Startwert 0
+summe = 0
+# Frage den User "Bitte Zahl x eingeben:" und gebe die Antwort in Variable x
+x = input("Bitte Zahl x eingeben:")
+# Frage den User "Bitte Zahl y eingeben:" und gebe die Antwort in Variable y
+y = input("Bitte Zahl y eingeben:")
+# Wandle x in einen Integer um und speichere es in x
+x = int(x)
+# Wandle y in einen Integer um und speichere es in y
+y = int(y)
+# Rechne x + y und gebe das Ergebnis in summe
+summe = x + y 
+# Sage dem User "Die Summe ist " summe 
+print("Die Summe ist: ", summe)
+
+
+
+# Kommentar
+'''
+Ich bin ein mehrzeiliger Kommentar
+'''
+
+# Erstelle eine Int-Variable für x mit dem Startwert 0
+# Erstelle eine Int-Variable für y mit dem Startwert 0
+# Erstelle eine Int-Variable für summe mit dem Startwert 0
+# Frage den User "Bitte Zahl x eingeben:" und gebe die Antwort in Variable x
+# Frage den User "Bitte Zahl y eingeben:" und gebe die Antwort in Variable y
+# Wandle x in einen Integer um und speichere es in x
+# Wandle y in einen Integer um und speichere es in y
+# Rechne x + y und gebe das Ergebnis in summe
+# Sage dem User "Die Summe ist " summe 
